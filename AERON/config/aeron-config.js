9d36f96486a6c2cd3ddbf74131aad57f4d22bc16
Devendra369369 <sadhuji9616@gmail.com>
2026-08-31 00:40:36 +0530

    Update AERON API deployment URL

--- AERON/config/aeron-config.js
+++ AERON/config/aeron-config.js
@@ -4,7 +4,7 @@
 "use strict";
 
 window.AERON_CONFIG = Object.freeze({
-    apiUrl: "https://script.google.com/macros/s/AKfycbxnDVZFqDXbrVXSGHfpn9eRRPTTR8B9RzDjAGE6SyqLxuf_Oq8qZvNloeSIaCwROHsqvw/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycbwHLdgDT9lyVnQc_65gF3LQC1ZWuFnfOU2Uxwg7PVee8O-oUGdDjcK29EBzCSHMtDSrxQ/exec",
     instituteName: "Surya Computer Of Education Center",
     instituteLocation: "Kamalpur, Chandauli, Uttar Pradesh",
     contactPhone: "7084275870",
