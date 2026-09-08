1472001e6b782ee29e4967718f05425dafa0f1c4
Devendra369369 <sadhuji9616@gmail.com>
2026-09-08 11:24:33 +0530

    Update AERON multi-AI backend and V42 API

--- AERON/config/aeron-config.js
+++ AERON/config/aeron-config.js
@@ -4,7 +4,7 @@
 "use strict";
 
 window.AERON_CONFIG = Object.freeze({
-    apiUrl: "https://script.google.com/macros/s/AKfycbzMlS5zOHjMK1nArFBqkuKFDLnrlRsT-YrVo8neCup13tn_x83uw19eqWLxl8jWF5qNtQ/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycby8xpCNc1q138G1qCgkdJ-CsHbc_r9wjUFI_w0_HHlvctwLD9jhECFF0iyVSOhXYiBSjQ/exec",
     instituteName: "Surya Computer Of Education Center",
     instituteLocation: "Kamalpur, Chandauli, Uttar Pradesh",
     contactPhone: "7084275870",
